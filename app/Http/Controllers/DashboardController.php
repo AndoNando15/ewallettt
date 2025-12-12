fccef15f4040c21957c2d33c7fae274d48273115
AndoNando15 <andonando158@gmail.com>
2025-12-12 21:39:43 +0700

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,14 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Dataset;
 class DashboardController extends Controller
 {
 
-
     public function index()
     {
+        // Hitung total dataset
+        $totalDataset = Dataset::count();
 
-        return view('pages.dashboard.index');
+        return view('pages.dashboard.index', compact('totalDataset'));
+
     }
+
 }
